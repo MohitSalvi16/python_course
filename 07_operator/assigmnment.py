b22c6c8225094c44dcead057feb1cf5563a1f10c
Mohit <salvimohit08@gmail.com>
2026-08-27 23:40:22 +0530

    progress

---
+++ 07_operator/assigmnment.py
@@ -0,0 +1,5 @@
+a=4
+print(a)
+a-=4
+
+print(a)
---
+++ 07_operator/operator.py
@@ -0,0 +1,44 @@
+a=34
+b=3
+
+print("a+b",a+b);
+print("a-b",a-b);
+print("a*b",a*b);
+print("a/b",a/b);
+print("a%b",a%b);
+print("a//b",a//b);
+print("a**b",a**b);
+
+print(a>4)
+print(a<4)
+print(a>=4)
+print(a<=4)
+print(a!=34)
+print(a==4)
+c=True
+d=False
+
+print("-----------")
+
+print(c and c)
+print(d or d)
+print(c and d)
+print(c or d)
+
+print("----and-------")
+
+print(True and True)
+print(False and False)
+print(True and False)
+print(False and True)
+
+print("------or-----")
+
+print(True or True)
+print(False or False)
+print(True or False)
+print(False or True)
+print("------Not-----")
+
+print(not(True))
+print(not(False))
