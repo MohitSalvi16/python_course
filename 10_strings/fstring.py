9c86353a7771485ac4f1d2adac5ca198c74bb70f
Mohit <salvimohit08@gmail.com>
2026-09-07 23:30:46 +0530

    progress

---
+++ 10_strings/fstring.py
@@ -0,0 +1,17 @@
+template= "Dear {}, you are awesome. Take this {}$ bag"
+
+
+a="john"
+a1=10222
+
+b="Ava"
+b1=3242
+
+c="chirag"
+c1=4323
+
+s1=template.format(c,c1)
+print(s1)
+
+
+print(f"{a} You are awesome give me a {a1}$ money")
