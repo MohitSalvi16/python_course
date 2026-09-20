5ed886fa2205f016548253d1709f04fd5ae9de68
Mohit <salvimohit08@gmail.com>
2026-09-21 00:43:11 +0530

    Progress

---
+++ DSA_problems/01_problem.py
@@ -0,0 +1,10 @@
+l=["apple","banana","cherry"]
+
+print(l[0])
+
+l[1]="orange"
+
+print(l)
+
+# print(l.len())
+print(len(l))
---
+++ DSA_problems/04_problem.py
@@ -0,0 +1,13 @@
+coordinates=(10,20)
+
+print(coordinates)
+
+# coordinates[0]=50
+
+
+l=list(coordinates)
+l[0]=50
+print(l)
+
+coordinates=tuple(l)
+print(coordinates)
