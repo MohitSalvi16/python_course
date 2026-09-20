4ea4c1082f3d87882cab4dbbbf18262d537b836c
Mohit <salvimohit08@gmail.com>
2026-09-20 15:16:21 +0530

    Progress

---
+++ 15_dictionaries/01_dictonary.py
@@ -0,0 +1,5 @@
+dict={"harry":34, "linda":55, "Mohit":26}
+
+dict["harry"]=3
+print(dict,type(dict))
+print(dict["harry"])
---
+++ 15_dictionaries/02_dict_method.py
@@ -0,0 +1,8 @@
+dict={"harry":34, "linda":55, "Mohit":26}
+
+print(dict.keys())
+print(dict.values())
+
+# dict.clear()
+dict.pop("linda")
+print(dict)
